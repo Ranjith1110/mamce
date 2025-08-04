9d9460f60244fccc7f4d93860513f35bf4490d37
Ranjith <ranjithram878@gmail.com>
2025-08-04 16:35:28 +0530

    first commit

--- resources/views/departments/ug/ai&ml.blade.php
+++ resources/views/departments/ug/ai&ml.blade.php
@@ -1,16 +1,7 @@
 @extends('includes.header')
 @section('content')
 
-<!-- Spinner Start here -->
-<div id="spinner"
-    class="bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-    <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-        <span class="sr-only">Loading...</span>
-    </div>
-</div>
-<!-- Spinner End here -->
-
-    <!-- Vertical Button Start here -->
+<!-- Vertical Button Start here -->
 <button style="background-color: #9d7651; border: 2px solid #dfcfc1;"
     class="vertical-button text-white px-md-2 px-0 py-2 rounded text-uppercase" data-bs-toggle="modal"
     data-bs-target="#admissionEnquiryModal" id="openPopupBtn">
--- resources/views/departments/ug/ece.blade.php
+++ resources/views/departments/ug/ece.blade.php
@@ -1,14 +1,6 @@
 @extends('includes.header')
 @section('content')
 
-<!-- Spinner Start here -->
-<div id="spinner"
-    class="bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-    <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-        <span class="sr-only">Loading...</span>
-    </div>
-</div>
-<!-- Spinner End here -->
 
 <!-- Vertical Button Start here -->
 <button style="background-color: #9d7651; border: 2px solid #dfcfc1;"
